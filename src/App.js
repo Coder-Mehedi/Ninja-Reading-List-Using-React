27c09cdb554d16e02e79b54bea6899ca9d199fcc
Coder-Mehedi <mehedi5051@gmail.com>
2019-10-10 20:10:55 +0600

    Dummy Calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import './App.css';
-import BookContextProvider from './contexts/BookContext';
-import Navbar from './components/Navbar';
-import BookList from './components/BookList';
-import BookForm from './components/BookForm';
+import React, { useState } from 'react'
+import './App.css'
+import BookContextProvider from './contexts/BookContext'
+import Navbar from './components/Navbar'
+import BookList from './components/BookList'
+import BookForm from './components/BookForm'
+import Calculator from './components/Calculator'
+
 
 function App() {
+  
   return (
     <div className="App">
       <BookContextProvider>
@@ -13,8 +16,9 @@ function App() {
         <BookList />
         <BookForm />
       </BookContextProvider>
+      <Calculator />
     </div>
   );
 }
 
-export default App;
+export default App
